2337916a3096b47e62b2f2378285741bfc0ba671
Aswin <122090298+Aswin-786@users.noreply.github.com>
2023-11-02 12:30:19 +0530

    Update deploy.sh

--- part-2-scripts/deploy.sh
+++ part-2-scripts/deploy.sh
@@ -1,32 +1,28 @@
 #!/bin/bash
 
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+
 # Change to the project directory
 cd /home/ubuntu/CI-CD # Update the directory path according to your project
-ls
+
+# Use NVM to install the required Node.js version
+nvm install 20.5.0
+
+# Use the installed Node.js version
+nvm use 20.5.0
 
 # Fetch the latest changes from the main branch of the Git repository
 git pull origin main
-ls
 
 # Change to the server directory
 cd server
-ls
-
-# Use 'which' command to get the path of node and npm
-NODE_PATH=$(which node)
-NPM_PATH=$(which npm)
-
-# Check if Node.js and npm are available
-if [ -z "$NODE_PATH" ] || [ -z "$NPM_PATH" ]; then
-    echo "Node.js or npm not found. Please ensure they are installed or adjust the paths."
-    exit 1
-fi
 
 # Install PM2 if not already available
-$NPM_PATH i -g pm2
+npm install pm2 -g
 
 # Terminate any existing running processes managed by PM2
-$(which pm2) kill
+pm2 kill
 
 # Start the Node.js application using PM2
-$(which pm2) start index.js
+pm2 start index.js
