2c0adaa5596fc8000caaf65dfaedaec3cbcbb222
Aswin <aswinkrishna363@gmail.com>
2023-11-02 13:29:48 +0530

    changed

--- server/index.js
+++ server/index.js
@@ -4,11 +4,11 @@ const app = express()
 app.use(express.json())
 
 app.get('/', (req, res) => {
-  res.json({ message: 'checkinh'})
+  res.json({ message: 'fi' })
 })
 
 app.get('/me', (req, res) => {
-  res.json({ message: 'find'})
+  res.json({ message: 'find' })
 })
 
 app.listen(4000, () => console.log(`Example app listening on port 4000`))
