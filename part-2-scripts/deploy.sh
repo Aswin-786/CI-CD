30a376600d4e5ffe32755cd128aa4bfe0ec4abe3
Aswin <122090298+Aswin-786@users.noreply.github.com>
2023-11-02 12:00:15 +0530

    Update deploy.sh

--- part-2-scripts/deploy.sh
+++ part-2-scripts/deploy.sh
@@ -4,5 +4,6 @@ export PATH=$PATH:/home/ubuntu/.nvm/versions/node/v20.5.0/bin
 cd CI-CD
  git pull origin main
  cd server
- pm2 kill
- pm2 start index.js
+ node index.js
+ #pm2 kill
+ #pm2 start index.js
