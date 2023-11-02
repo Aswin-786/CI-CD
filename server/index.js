59dd3e026c4c8102984154d4a04e61ba11947d38
Aswin <122090298+Aswin-786@users.noreply.github.com>
2023-11-02 13:42:33 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const app = express()
 app.use(express.json())
 
 app.get('/', (req, res) => {
-  res.json({ message: 'whaaaaaaaat' })
+  res.json({ message: 'godaaa' })
 })
 
 app.get('/me', (req, res) => {
