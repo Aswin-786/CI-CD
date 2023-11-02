f4a3c206ee0218280aa5c9b9a7e60937490c8715
Aswin <122090298+Aswin-786@users.noreply.github.com>
2023-11-02 12:25:28 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const app = express()
 app.use(express.json())
 
 app.get('/', (req, res) => {
-  res.json({ message: '1' })
+  res.json({ message: '22' })
 })
 
 app.listen(4000, () => console.log(`Example app listening on port 4000`))
