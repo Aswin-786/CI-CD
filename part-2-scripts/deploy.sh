6670b2c8253ad59bb3cf3ab23a40f03e7328149f
Aswin <122090298+Aswin-786@users.noreply.github.com>
2023-11-02 12:17:00 +0530

    Update deploy.sh

--- part-2-scripts/deploy.sh
+++ part-2-scripts/deploy.sh
@@ -5,12 +5,13 @@ export PATH=$PATH:/home/ubuntu/.nvm/versions/node/vXX.X.X/bin # Update the Node.
 
 # Change to the project directory
 cd /home/ubuntu/CI-CD # Update the directory path according to your project
-
+ls
 # Fetch the latest changes from the main branch of the Git repository
 git pull origin main
-
+ls
 # Change to the server directory
 cd server
+ls
 npm i -g pm2
 # Terminate any existing running processes managed by PM2
 pm2 kill
