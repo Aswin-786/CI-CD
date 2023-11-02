8980f5f18dda51208ad9fb26524c55704d9fb03f
Aswin <122090298+Aswin-786@users.noreply.github.com>
2023-11-02 13:45:59 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -4,11 +4,11 @@ const app = express()
 app.use(express.json())
 
 app.get('/', (req, res) => {
-  res.json({ message: 'now i made this aswin ... you are a hardworker keep pushing yourself dude ' })
+  res.json({ message: 'yesssssssssssssssssssss now i made this aswin ... you are a hardworker keep pushing yourself dude ' })
 })
 
 app.get('/me', (req, res) => {
-  res.json({ message: '.. you are going to win' })
+  res.json({ message: '.......you are going to win.....' })
 })
 
 app.listen(4000, () => console.log(`Example app listening on port 4000`))
