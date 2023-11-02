f312b084098c79ab4510d9d06ab22ac6c12b25e9
Aswin <122090298+Aswin-786@users.noreply.github.com>
2023-11-02 13:18:38 +0530

    Update deploy.sh

--- part-2-scripts/deploy.sh
+++ part-2-scripts/deploy.sh
@@ -3,8 +3,14 @@ export PATH=$PATH:/home/ubuntu/.nvm/versions/node/v20.5.0/bin
 
 cd CI-CD
  git pull origin main
+ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+ export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+ nvm install node
+ npm i
+ npm i -g pm2
  cd server
  git pull origin main
  pm2 kill
  pm2 start index.js
-
