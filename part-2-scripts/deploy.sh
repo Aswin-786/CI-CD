c6e295c405fc6da7d5bc2c34f99e062ac3208c3b
Aswin <122090298+Aswin-786@users.noreply.github.com>
2023-11-02 12:25:12 +0530

    Update deploy.sh

--- part-2-scripts/deploy.sh
+++ part-2-scripts/deploy.sh
@@ -1,20 +1,25 @@
 #!/bin/bash
 
-# Ensure the correct Node.js environment
+# Set the correct Node.js environment path
 export PATH=$PATH:/home/ubuntu/.nvm/versions/node/v20.5.0/bin # Update the Node.js path accordingly
 
 # Change to the project directory
 cd /home/ubuntu/CI-CD # Update the directory path according to your project
 ls
+
 # Fetch the latest changes from the main branch of the Git repository
 git pull origin main
 ls
+
 # Change to the server directory
 cd server
 ls
-npm i -g pm2
+
+# Ensure the correct PM2 path (if different from Node.js)
+/home/ubuntu/.nvm/versions/node/v20.5.0/bin/npm i -g pm2
+
 # Terminate any existing running processes managed by PM2
-pm2 kill
+/home/ubuntu/.nvm/versions/node/v20.5.0/bin/pm2 kill
 
 # Start the Node.js application using PM2
-pm2 start index.js
+/home/ubuntu/.nvm/versions/node/v20.5.0/bin/pm2 start index.js
