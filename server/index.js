d1e1f1f0251080fd1e9f232d404e25133be8fd82
Aswin <122090298+Aswin-786@users.noreply.github.com>
2023-11-05 22:14:19 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -4,11 +4,11 @@ const app = express()
 app.use(express.json())
 
 app.get('/', (req, res) => {
-  res.json({ message: 'oh this is too good this ci cd' })
+  res.json({ message: 'my ci cd is working' })
 })
 
 app.get('/me', (req, res) => {
-  res.json({ message: 'ok' })
+  res.json({ message: 'you are going to win aswin...' })
 })
 
 app.listen(4000, () => console.log(`Example app listening on port 4000`))
